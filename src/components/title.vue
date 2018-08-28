b42c33db44f892d795091dd2478d9d7665421f68
wangtao <wangtao1@zeyihuoyun.com>
2018-08-28 10:17:25 +0800

    0828 1017

---
+++ src/components/title.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="index_title">
+    <img src="../../assets/title.png">
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'title',
+    data() {
+      return {}
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .index{
+    &_title{
+      font-size: 20px;
+    }
+  }
+</style>
