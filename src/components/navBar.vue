64b685ec76595cdcc248f18aee2b4ca22da4d212
wangtao <wangtao1@zeyihuoyun.com>
2018-08-29 09:43:08 +0800

    0829 0942

--- src/components/navBar.vue
+++ src/components/navBar.vue
@@ -1,18 +1,18 @@
 <template>
   <el-row style="width:720px; margin: 20px auto;">
-    <el-col :span="4">
-      <router-link to="index">经营分析</router-link>
+    <el-col :span="4" :class="{active: index==0}" @click="index=0">
+      <router-link :to="{path:'/index',query:{index:0}}">经营分析</router-link>
     </el-col>
-    <el-col :span="4"  :offset="1">
-      <router-link to="show">园区展示</router-link>
+    <el-col :span="4" :class="{active: index==1}" @click="index=1"  :offset="1">
+      <router-link :to="{path:'/show',query:{index:1}}">园区展示</router-link>
     </el-col>
-    <el-col :span="4"  :offset="1">
-      <router-link to="management">园区管控</router-link>
+    <el-col :span="4" :class="{active: index==2}" @click="index=2"  :offset="1">
+      <router-link :to="{path:'/management',query:{index:2}}">园区管控</router-link>
     </el-col>
-    <el-col :span="4"  :offset="1">
+    <el-col :span="4" :class="{active: index==3}" @click="index=3"  :offset="1">
       <router-link to="">孵化管理</router-link>
     </el-col>
-    <el-col :span="4"  :offset="1">
+    <el-col :span="4" :class="{active: index==4}" @click="index=4"  :offset="1">
       <router-link to="">视频监控</router-link>
     </el-col>
   </el-row>
@@ -21,7 +21,12 @@
 <script>
 export default {
   data () {
-    return {}
+    return {
+      index: 0
+    }
+  },
+  mounted () {
+    this.index = this.$route.query.index
   }
 }
 </script>
@@ -43,7 +48,8 @@ export default {
       text-decoration: none;
     }
   }
-  .el-col:hover {
+  .el-col:hover,
+  .el-col.active{
     background-image: url("/static/btn_active.png");
     a {
       color: #bb4b00;
