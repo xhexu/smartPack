bacf87325495d221910220c7ea22b1b0e7e1b2ed
wanto2014 <ssswaaangtaosss@163.com>
2018-08-29 23:36:17 +0800

    0829晚--页面

--- src/views/parkManagement/index.vue
+++ src/views/parkManagement/index.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
     <el-row>
-      <el-col :span="7">1</el-col>
-      <el-col :span="10">
+      <el-col :span="7" class="left">
+        <div class="box">
+          <p>本栋共4层</p>
+          <p>入驻企业6家</p>
+          <p>企业类型包含餐饮，金融，IT等</p>
+          <span class="btn">进入本栋</span>
+        </div>
+      </el-col>
+      <el-col :span="10" class="center">
         <img class="imgT" src="../../assets/top_bar.png"/>
-        <div class="view"></div>
+        <div class="view">
+          <img src="../../assets/building.gif"/>
+        </div>
         <img class="imgB" src="../../assets/top_bar.png"/>
       </el-col>
-      <el-col :span="7">1</el-col>
+      <el-col :span="7" class="right">
+        <div class="box">
+          <p>本栋共12层</p>
+          <p>入驻企业62家</p>
+          <p>企业类型包含餐饮，金融，制造，IT，服务业等</p>
+          <span class="btn">进入本栋</span>
+        </div>
+      </el-col>
     </el-row>
     <el-row>
       <el-col :span="7">&nbsp;</el-col>
@@ -34,7 +50,18 @@
 </script>
 
 <style lang="scss" scoped>
-  .imgT{width:100%;}
-  .view{width:100%;height:450px;}
-  .imgB{width:100%;-moz-transform:rotate(180deg);-webkit-transform:rotate(180deg);}
+  .box{
+    background: url('/static/divTL.png') no-repeat;background-size: 100% 100%;width:40%;font-size: 14px;color:#01A4AE;font-weight: bold;padding:100px 0 50px 80px;text-align: left;
+    span.btn{display: block;width:156px; height:44px; margin: 30px auto 0; line-height: 44px;text-align: center;background: url('/static/bg_btn.png') no-repeat;background-size: 100% 100%;}
+  }
+  .left{}
+  .center{
+    .imgT{width:100%;}
+    .view{
+      width:95%;margin: -15px auto;background:url('/static/bg_view.png') no-repeat center;background-size: 100% 100%;vertical-align: middle;
+      img{width:100%;margin: 80px auto 10px;}
+    }
+    .imgB{width:100%;-moz-transform:rotate(180deg);-webkit-transform:rotate(180deg);}
+  }
+  .right{}
 </style>
