6f3f3af2268ea21403ed87892aee24ce0b8f67f6
01127856 <01127856@yto.net.cn>
2018-08-30 10:05:21 +0800

    hatch,monitor

---
+++ src/views/parkHatch/Card.vue
@@ -0,0 +1,143 @@
+<template>
+	<div class="card" :style="{backgroundImage: 'url(' + cardBgUrl + ')'}">
+		<div class="card-title" :class="titleClass">{{title}}</div>
+		<div class="card-div" :class="cardDivClass" >
+			<ul>
+				<li v-for="item in info">
+					{{item.title}}
+			    </li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+	import _ from 'underscore'
+	export default {
+		name: 'hatchCard',
+		props: {
+		    option: {
+		      type: Object,
+		      default () {
+		        return {}
+		      }
+		    },
+		    info: {
+		    	type: Array,
+		    	default () {
+			      return []
+			    }
+		    }
+		},
+		data () {
+			return {
+				showItemDiv: false,
+				cardDivClass: '',
+				itemClass: '',
+				titleClass: '',
+				chartClass: '',
+				cardBgUrl: '',
+				title: '-----'
+			}
+		},
+		mounted () {
+			this.dealChartOption()
+			this.dealCardOption()
+		},
+		methods: {
+			dealChartOption () {
+				if(_.isObject(this.chart)){
+					this.chartOption = this.chart
+				}
+			},
+			dealCardOption () {
+				if(_.isObject(this.option)){
+					if(!this.option.position){
+						return;
+					}
+					if(this.option.title){
+						this.title = this.option.title;
+					}
+					if(_.isObject(this.option.item)){
+						_.extend(this.item,this.option.item)
+					}
+					switch(this.option.position){
+						case 'TL':
+						  this.cardBgUrl = '/static/hatch_car_bg.png'
+						  this.titleClass = 'card-title-tl'
+						  this.cardDivClass = 'card-div-tl'
+						  break;
+						case 'TR':
+						  this.cardBgUrl = '/static/hatch_car_right.png'
+						  this.titleClass = 'card-title-tr'
+						  this.cardDivClass = 'card-div-tl'
+						  break;
+						default:
+						  console.error('卡片未匹配到')
+					}
+				}else{
+					console.error('卡片入参异常')
+				}
+			}
+		}
+
+	}
+
+</script>
+
+<style lang="scss" scoped>
+	.card{
+		position: relative;
+        background-repeat: no-repeat;
+        background-size:100% 100%;
+        height: 300px;
+        max-width: 260px;
+        margin: 0 auto;
+        &-item{
+        	position: absolute;
+        	color:#01A4AE;
+        	font-size: 14px;
+        	cursor: pointer;
+        	transition: all .5s;
+			-moz-transition: all .5s;
+			-webkit-transition: all .5s;
+			-o-transition: all .5s;
+        }
+        &-div{
+        	position: absolute;
+		    li {
+		    	display: block;
+		    	color: #01A4AE;
+		    	cursor: pointer;
+		    	font-size: 18px;
+		    }
+		    li:hover{
+		    	color: #EB850C;
+		    }
+        }
+        &-div-tl{
+		    left: 10px;
+		    top: 40px;
+        }
+        &-title{
+        	position: absolute;
+        	color: #01A4AE;
+        }
+        &-title-tl{
+        	height:30px;
+        	width:94px;
+        	left: 15px;
+        	top: 15px !important;
+        	padding-top: 10px;
+        	background:transparent url('/static/hatch_but_bg.png') no-repeat center;
+        }
+        &-title-tr{
+        	height:30px;
+        	width:94px;
+        	right: 15px;
+        	top: 15px !important;
+        	padding-top: 10px;
+        	background:transparent url('/static/hatch_but_bg.png') no-repeat center;
+        }
+	}
+</style>
---
+++ src/views/parkHatch/index.vue
@@ -0,0 +1,224 @@
+<template>
+  <div class="index">
+    <!--内容区域-->
+    <div id="main">
+      <div class="main-left">
+        <card :option="cardTL" :info="optionTL"></card>
+        <card :option="cardBL" :info="optionBL"></card>
+      </div>
+
+      <!--视频播放区-->
+      <div class="main-center">
+        <div class="main-center_video" >
+          <img style="margin-top: 250px;" :class="{animTop:isPlayAnimation}" src="../../assets/top_bar.png">
+          <div class="videoMap" :class="{videoMapAnim:isShowVideo}"></div>
+          <img style="-moz-transform:rotate(180deg);-webkit-transform:rotate(180deg);" :class="{animBtm:isPlayAnimation}" src="../../assets/top_bar.png">
+        </div>
+        <div class="main-center_navbar">
+          <nav-bar></nav-bar>
+        </div>
+      </div>
+
+      <div class="main-right">
+        <card :option="cardTR" :info="optionTR"></card>
+        <card :option="cardBR" :info="optionBR"></card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import navBar from '../../components/navBar.vue'
+import Card from './Card.vue'
+
+
+export default {
+  name: 'IndexPage',
+  components: {
+    navBar,
+    Card
+  },
+  data () {
+    return {
+      isPlayAnimation:false,
+      isShowVideo: false,
+      cardTL: {
+        title:'政策',
+        position: 'TL'
+      },
+      cardTR: {
+        title:'活动',
+        position: 'TR'
+      },
+      cardBL: {
+        title:'孵化器',
+        position: 'TL'
+      },
+      cardBR: {
+        title:'指南',
+        position: 'TR'
+      },
+      optionTL: [{
+        title:'xxxx',
+        detail:'aaaaaaa',
+        time:'2018-7-23'
+      },{
+        title:'xxxx',
+        detail:'bbbbbbbb',
+        time:'2018-7-23'
+      },{
+        title:'xxxx',
+        detail:'cccccccc',
+        time:'2018-7-23'
+      }],
+      optionBL: [{
+        title:'xxxx',
+        detail:'aaaaaaa',
+        time:'2018-7-23'
+      },{
+        title:'xxxx',
+        detail:'bbbbbbbb',
+        time:'2018-7-23'
+      },{
+        title:'xxxx',
+        detail:'cccccccc',
+        time:'2018-7-23'
+      }],
+      optionBR: [{
+        title:'xxxx',
+        detail:'aaaaaaa'
+      },{
+    title:'xxxx',
+        detail:'bbbbbbbb'
+      },{
+    title:'xxxx',
+        detail:'cccccccc'
+      }],
+      optionTR: [{
+        title:'xxxx',
+        detail:'aaaaaaa'
+      },{
+    title:'xxxx',
+        detail:'bbbbbbbb'
+      },{
+    title:'xxxx',
+        detail:'cccccccc'
+      }]
+    }
+  },
+  methods: {
+    /**
+     * 查询数据
+     * @return {[type]} [description]
+     */
+    queryChartData () {
+      
+    },
+    playAanimation () {
+      let me = this
+      this.isPlayAnimation = !this.isPlayAnimation
+      setTimeout(function(){
+        me.isShowVideo = !me.isShowVideo
+      },200)
+    }
+  },
+  beforeCreate () {
+
+  },
+  beforeMount () {
+    this.isPlayAnimation = false
+    this.isShowVideo = false
+  },
+  mounted () {
+    //查询首页数据
+    this.queryChartData()
+    this.playAanimation()
+    let me = this
+     /**setInterval(function(){
+     me.optionTR.series = [{
+          data:[parseInt(Math.random()*1000), parseInt(Math.random()*1000), parseInt(Math.random()*1000), parseInt(Math.random()*1000), parseInt(Math.random()*1000), parseInt(Math.random()*1000), parseInt(Math.random()*1000)]
+      }];
+    },5000)
+
+    setInterval(function(){
+      me.optionBR.series = [{
+          data:[parseInt(Math.random()*1000), parseInt(Math.random()*1000), parseInt(Math.random()*1000), parseInt(Math.random()*1000), parseInt(Math.random()*1000), parseInt(Math.random()*1000), parseInt(Math.random()*1000)]
+      }];
+    },10000)**/
+  }
+}
+</script>
+
+<!-- 首页样式 -->
+<style lang="scss" scoped>
+.index{
+  max-width: 1366px;
+  margin: 0 auto;
+  &_title{
+    font-size: 20px;
+  }
+  #main{
+    width: 100%;
+    .main-left{
+      position: relative;
+      top: -55px;
+      width:25%;
+      min-height:800px;
+      float: left;
+    }
+    .main-center{
+      width:50%;
+      height:800px;
+      float:left;
+      position: relative;
+      img{
+        width:90%;
+        -webkit-transition:all .5s ease .5s;
+        transition: all .5s ease .5s;
+      }
+      &_video{
+        min-height:300px;
+      }
+      &_navbar{
+        position:absolute;
+        left:-40px;
+      }
+      .animTop{
+        margin-top:50px !important;
+      }
+      .animBtm{
+        margin-top:360px !important;
+      }
+      .videoMap{
+        width: 85%;
+        margin-left: 50px;
+        top: 64px;
+        height: 49%;
+        position: absolute;
+        background:transparent url('/static/hatch_center.gif') no-repeat center;
+        background-size: 100% 100%;
+        opacity: 0;
+        transition: all .5s ease .5s;
+        -webkit-transition:all .5s ease .5s;
+      }
+      .videoMapAnim{
+        opacity: 1;
+      }
+    }
+    .main-right{
+      position: relative;
+      top: -50px;
+      width:25%;
+      height:800px;
+      float:right;
+    }
+  }
+  &_map{
+    min-height: 300px;
+    width: 90%;
+  }
+  &_btns{
+
+  }
+}
+</style>
