aab8f3891ab261ea06dcac8f541232a173766e1e
wangtao <wangtao1@zeyihuoyun.com>
2018-08-30 15:48:42 +0800

    0830下午

---
+++ src/views/parkManagement/buildingA.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-row>
+    <el-col :span="7" :offset="2"></el-col>
+    <el-col :span="13" :offset="2"></el-col>
+  </el-row>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
