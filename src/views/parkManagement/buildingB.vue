b568d96c50d0fd1b47cdfd33b5469a35cc8fee78
wangtao <wangtao1@zeyihuoyun.com>
2018-10-09 09:12:03 +0800

    页面样式调整

--- src/views/parkManagement/buildingB.vue
+++ src/views/parkManagement/buildingB.vue
@@ -369,7 +369,7 @@ export default {
         color:white; font-size:14px;text-indent: 30px;width:95%;height:20px;line-height: 20px;
       }
       #chart{
-        position: absolute;left:5%;bottom:10%;height: 45%;width:90%;color:white!important;
+        position: absolute;left:1%;bottom:10%;height: 45%;width:98%;color:white!important;
       }
     }
     .btns{
