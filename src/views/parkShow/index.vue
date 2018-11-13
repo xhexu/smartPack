70fe70529cdd268d5cf86d59c32c60fab57fa443
xunfeng1122 <xunfeng1122@163.com>
2018-11-13 15:31:34 +0800

    监控

--- src/views/parkShow/index.vue
+++ src/views/parkShow/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="layout">
-    <iframe style="width:88%; height:92%; margin: auto;" src="http://www.360vrpano.com/index.php/Project/viewProject/id/28985.html"></iframe>
+    <iframe style="width:88%; height:92%; margin: auto;" src="http://www.360vrpano.com/index.php/Project/viewProject/id/29963.html"></iframe>
   </div>
 </template>
 
